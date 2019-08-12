eb25608c2a1ab6f62dcf5cddd2a3c0549dfeb187
Mohit Mishra <mmishra@sangoma.com>
2019-08-12 11:04:54 +0000

    FREEPBX-20422 Restoring FreePBX 15 system is not restoring Queue Priority entries.
    	Fixed.

--- Backup.php
+++ Backup.php
@@ -5,7 +5,7 @@ class Backup Extends Base\BackupBase{
 	public function runBackup($id,$transaction){
 		$this->addDependency('queues');
 		$this->addConfigs([
-			'tables' => $this->dumpTables(),
+			'tables' => $this->dumpDBTables('queueprio',false),
 			'features' => $this->dumpFeatureCodes(),
 			'settings' => $this->dumpAdvancedSettings()
 		]);
